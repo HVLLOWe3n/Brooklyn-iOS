710037b64cbe528529fddd7d20da4588b54f02d4
Roman Synovets <Roman.Synovets@gmail.com>
2018-06-15 15:32:01 +0300

    create projects

---
+++ Brooklyn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brooklyn
+//
+//  Created by Roman Synovets on 6/14/18.
+//  Copyright © 2018 Roman Synovets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
